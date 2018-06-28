974a85940a71e91bf96dffe6d4b5825981b9b1a0
Will in China <email.will.in.china@gmail.com>
2018-06-28 11:48:02 +0800

    version one

---
+++ blox-print.js
@@ -0,0 +1,60 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `blox-print`
+ * A component that prints the given HTML
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class BloxPrint extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      error: {
+        type: String,
+        notify: true,
+        reflectToAttribute: true,
+      },
+      html: {
+        type: String,
+        observer: '_print'
+      },
+    };
+  }
+
+  _print(){
+    this.print(this.html)
+    .catch((err) => {
+      this.error = err;
+    })
+  }
+  print(htmlToPrint){
+    return new Promise((resolve, reject) => {
+      console.log(htmlToPrint)
+      var printWindow=window.open('','','width=200,height=100');
+      printWindow.document.write(htmlToPrint);
+      printWindow.document.close();
+      printWindow.focus();
+      setTimeout(function () { 
+        printWindow.print(); 
+      }, 500);
+      printWindow.onfocus = function () { 
+        setTimeout(function () { 
+          printWindow.close(); 
+        }, 500); 
+      }
+    })
+  }
+
+} window.customElements.define('blox-print', BloxPrint);
